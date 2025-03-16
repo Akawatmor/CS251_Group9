1737c17b0381c9d443af8071c3687d1ec49aec89
Akawatmor <akawat.mor@dome.tu.ac.th>
2025-03-16 20:31:37 +0700

    Init

---
+++ backend/src/test/java/cs251/group9/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package cs251.group9.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
