ed72c1c3b3519da313241258d74106c77ca94dde
Akawat Moradsatian - 6609681231 <144298634+Akawatmor@users.noreply.github.com>
2025-05-10 10:09:30 +0700

    f

--- backend/src/main/java/cs251/group9/backend/controller/FriendController.java
+++ backend/src/main/java/cs251/group9/backend/controller/FriendController.java
@@ -1,16 +1,23 @@
 /*
- * Friend controller
+ * FriendController.java
+ * 
+ * This class is a Spring Boot REST controller that handles requests related to
+ * friends.
+ * It provides endpoints to add a friend.
+ * 
  */
+package cs251.group9.backend.controller;
+
 import org.springframework.beans.factory.annotation.*;
 import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.*;
+//import org.springframework.web.multipart.*;
 
 import cs251.group9.backend.entity.*;
 //import cs251.group9.backend.service.*;
 import cs251.group9.backend.repository.*;
 
-import java.util.*;
+//import java.util.*;
 
 
 @RestController
